95d06f1d5a145e8029462ab87b7715151d1ea078
unluckmeba <jantayt2006@gmail.com>
2023-01-20 19:01:26 +0600

    hw3

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from aiogram.utils import executor
 import logging
 from config import dp
 from handlers import admin, callback, client, extra, fsm_anketa
+from  Database.bot_db import sql_create
+
+
+async def on_startup(_):
+    sql_create()
 
 callback.register_handlers_callback(dp)
 client.register_handlers_client(dp)
+fsm_anketa.register_handlers_fsm_anketa(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
-
 
 
 
